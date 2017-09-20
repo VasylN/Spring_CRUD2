90b8e8ff4a1ed382a833b02ba234581623227e21
VasylN <vasyln86@gmail.com>
2017-09-20 21:58:10 +0300

    Initial commit

---
+++ src/main/java/com/crud/main.java
@@ -0,0 +1,7 @@
+package com.crud;
+
+/**
+ * Created by Pc on 13.09.2017.
+ */
+public class main {
+}
---
+++ src/main/java/com/crud/model/User.java
@@ -0,0 +1,117 @@
+package com.crud.model;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.sql.Timestamp;
+
+/**
+ * Created by Pc on 13.09.2017.
+ */
+
+@Entity
+@Table(name = "students2")
+
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    private int id;
+
+    @Basic
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    @Basic
+    @Column(name = "age")
+    @NotNull
+    private Integer age;
+
+    @Basic
+    @Column(name = "isAdmin")
+    @NotNull
+    private Boolean isAdmin;
+
+    @Basic
+    @Column(name = "createdDate")
+    @GeneratedValue
+    @DateTimeFormat(pattern = "MM/dd/yyyy")
+    private Timestamp createdDate;
+
+    public User() {
+    }
+
+    public User(String name, Integer age, Boolean isAdmin) {
+        this.name = name;
+        this.age = age;
+        this.isAdmin = isAdmin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        if (age != null ? !age.equals(user.age) : user.age != null) return false;
+        if (isAdmin != null ? !isAdmin.equals(user.isAdmin) : user.isAdmin != null) return false;
+        return createdDate != null ? createdDate.equals(user.createdDate) : user.createdDate == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        result = 31 * result + (isAdmin != null ? isAdmin.hashCode() : 0);
+        result = 31 * result + (createdDate != null ? createdDate.hashCode() : 0);
+        return result;
+    }
+}
